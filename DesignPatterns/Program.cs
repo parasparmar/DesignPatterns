52537dac7d7935709c1b44ef752499f62cb7774f
Paras Parmar <parasparmar@gmail.com>
2021-07-18 00:07:18 +0530

    Add project files.

---
+++ DesignPatterns/Program.cs
@@ -0,0 +1,24 @@
+﻿using DesignPatterns.Creational.Builder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace DesignPatterns
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var builder = new HtmlBuilder("ul");
+            builder.AddChild("li", "Paras");
+            builder.AddChild("li", "Pratik");
+            builder.AddChild("li", "Ruby");
+            WriteLine(builder.ToString());
+            Read();            
+        }
+    }
+}

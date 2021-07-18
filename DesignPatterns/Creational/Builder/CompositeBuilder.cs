de3b837a12f5ca299d52f588c3bf23db7e2c145d
Paras Parmar <parasparmar@gmail.com>
2021-07-19 01:14:01 +0530

    Updated

--- DesignPatterns/Creational/Builder/CompositeBuilder.cs
+++ DesignPatterns/Creational/Builder/CompositeBuilder.cs
@@ -16,12 +16,16 @@ namespace DesignPatterns.Creational.Builder
         // Employment Information
         public string CompanyName, Position;
         public int AnnualIncome;
+        public override string ToString()
+        {
+            return $"{nameof(StreetAddress)}: {StreetAddress}, {nameof(PostCode)}: {PostCode}, {nameof(City)}: {City}, {nameof(CompanyName)}: {CompanyName}, {nameof(Position)}: {Position}, {nameof(AnnualIncome)}: {AnnualIncome}";
+        }
     }
 
     public class PersonBuilder
     {
+        // the object we're going to build
         protected Person person;
-
         public PersonBuilder() => person = new Person();
         protected PersonBuilder(Person person) => this.person = person;
         public PersonAddressBuilder Lives => new PersonAddressBuilder(person);
@@ -30,14 +34,47 @@ namespace DesignPatterns.Creational.Builder
 
     }
 
-    public class PersonJobBuilder
+    public class PersonJobBuilder: PersonBuilder
     {
+        public PersonJobBuilder(Person person): base(person)
+        {
+
+        }
+        public PersonJobBuilder AtCompany(string companyName)
+        {
+            person.CompanyName = companyName;
+            return this;
+        }
+        public PersonJobBuilder WithDesignation(string position)
+        {
+            person.Position = position;
+            return this;
+        }
+        public PersonJobBuilder Earning(int annualIncome)
+        {
+            person.AnnualIncome = annualIncome;
+            return this;
+        }
     }
 
     public class PersonAddressBuilder : PersonBuilder
     {
+        //The constructor takes as reference the base PersonBuilder it inherits the Person from. 
         public PersonAddressBuilder(Person person) : base(person) => this.person = person;
-
-
+        public PersonAddressBuilder AtStreet(string streetName)
+        {
+            person.StreetAddress = streetName;
+            return this;
+        }
+        public PersonAddressBuilder WithPostCode(string PostCode)
+        {
+            person.PostCode = PostCode; 
+            return this;
+        }
+        public PersonAddressBuilder InCity(string City)
+        {
+            person.City = City;
+            return this;
+        }
     }
 }
--- DesignPatterns/Program.cs
+++ DesignPatterns/Program.cs
@@ -31,13 +31,28 @@ namespace DesignPatterns
             //Read();
             //#endregion
 
-            #region Communicating Intent : Fluent Builder
-            var comm = HtmlElement
-                .Create("ul")
-                .AddChildFluent("li", "Paras")
-                .AddChildFluent("li", "Pratik")
-                .AddChildFluent("li", "Ruby");
-            WriteLine(comm);
+            //#region Communicating Intent : Fluent Builder
+            //var comm = HtmlElement
+            //    .Create("ul")
+            //    .AddChildFluent("li", "Paras")
+            //    .AddChildFluent("li", "Pratik")
+            //    .AddChildFluent("li", "Ruby");
+            //WriteLine(comm);
+            //Read();
+            //#endregion
+
+            #region Composite Builder
+            var pb = new PersonBuilder();
+            Person person = pb
+                .Lives
+                .AtStreet("63-64 Yoginagar")
+                .InCity("Borivali(W), Mumbai")
+                .WithPostCode("400092")
+                .Works
+                .AtCompany("Kale Logistics Solutions Pvt. Ltd.")
+                .WithDesignation("Lead Analyst")
+                .Earning(100);
+            WriteLine(person);
             Read();
             #endregion
         }
